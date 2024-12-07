87f59a6248928c8fca5c553e671a8da0303f0baa
ivan belkov <37552491-corndogycal@users.noreply.replit.com>
2024-12-07 07:23:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Ваня")
+print(12)
+print('Это викторина на знание кухни разных народов. Ответь на вопросы и узнай, насколько хорошо ты разбираешься в еде!')
+question1 = "Кто придумал эчпочмак?"
+question2 = "Луковый суп — это блюдо какой кухни?"
+question3 = "Где родина начос?"
+question4 = "Как называются китайские пельмени?"
+question5 = "В национальную кухню какой страны входят драники?"
+true_answer1 = "татары"
+true_answer2 = "Франция"
+true_answer3 = "Мексика"
+true_answer4 = "гёдза"
+true_answer5 = "Беларусь"
+answer1 = input(question1)
+answer2 = input(question2)
+answer3 = input(question3)
+answer4 = input(question4)
+answer5 = input(question5)
